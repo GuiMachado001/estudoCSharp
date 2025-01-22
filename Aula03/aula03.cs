e91f87d9370dfcc40184737f79dc6e899f811d06
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-01-22 11:36:19 -0400

    aula

--- Aula03/aula03.cs
+++ Aula03/aula03.cs
@@ -1,7 +1,22 @@
 using System;
 
 class Aula03{
-    static voin Main(){
+    static void Main(){
+        int n1 = -10;
+        char letra = 'c';
+        float valor = 1.10f;
+        string nome = "Guilherme";
+        var aux = 10; // var nao Específica a variavel
 
+        var aux2 = nome;
+
+        byte n2 = 10; // entre 0 e 255
+
+        int num1, num2, res;
+        num1 = 10;
+        num2 = 2;
+        res = num1 * num2;
+
+        Console.WriteLine("A multiplicação de " +num1+ "*" +num2+ " é igual a: " +res);
     }
 }
